8ec06cb1ea5531e1e0c932f5d07c2d08f6f3d4c2
amanda <amandaleticiaraymundo@gmail.com>
2024-10-11 09:00:26 -0300

    continuou

--- pulapassarinho/MainPage.xaml.cs
+++ pulapassarinho/MainPage.xaml.cs
@@ -2,23 +2,61 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	const int gravidade = 1;
+	const int tempoentreframes=25;
+	bool estamorto = true;
+	double LarguraJanela = 0;
+	double AlturaJanela = 0;
+	int velocidade = 20;
+
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+    void AplicarGravidade()
+	{
+		imgPersonagem.TranslationY +=gravidade;
+	}
 
-	///private void OnCounterClicked(object sender, EventArgs e)
-	//{
-		//count++;
+	async Task Desenha()
+	{
+		while (estamorto)
+		{
+			AplicarGravidade();
+			await Task.Delay(tempoentreframes);
+			GerenciaCanos();
+		}
+	}
+	
+	void onGameOverClicked(object s, TappedEventArgs e)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenha();
+	}
 
-		//if (count == 1)
-			//CounterBtn.Text = $"Clicked {count} time";
-		//else
-			//.Text = $"Clicked {count} times";
+	void Inicializar()
+	{
+		estamorto = false;
+		imgPersonagem.TranslationY = 0;
+	}
 
-		//SemanticScreenReader.Announce(CounterBtn.Text);
-	//}
+    protected void OnSizeAllocated (double w, double h)
+	{
+		base.OnSizeAllocated(w,h);
+		LarguraJanela = w;
+		AlturaJanela = h;
+	}
+	void GerenciaCanos()
+	{
+		ImgMorrinhoCima.TranslationX -=velocidade;
+		imgMorrinhoBaixo.TranslationX -=velocidade;
+		if(imgMorrinhoBaixo.TranslationX<-LarguraJanela)
+		{
+			imgMorrinhoBaixo.TranslationX =0;
+			ImgMorrinhoCima.TranslationX =0;
+		}
+	}
 }
 
